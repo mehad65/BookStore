bd1aff07720d63d3e3d04f497ab2512317502cfb
Md Mihadul Islam Sinin <mehad65@gmail.com>
2021-10-12 22:44:53 +0600

    Add MVC to Empty Project

---
+++ Bookstore/Bookstore/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "This is from my naem";
+        }
+    }
+}
--- Bookstore/Bookstore/Startup.cs
+++ Bookstore/Bookstore/Startup.cs
@@ -16,6 +16,7 @@ namespace Bookstore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +27,12 @@ namespace Bookstore
                 app.UseDeveloperExceptionPage();
             }
 
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
